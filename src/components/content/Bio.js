7c0129374fe55113f91efa5cf629f1c2af20abca
Lior Koren <liorkrn@gmail.com>
2022-11-13 13:43:03 +0200

    made a working desktop and mobile version

--- src/components/content/Bio.js
+++ src/components/content/Bio.js
@@ -2,7 +2,7 @@ import classes from "./Bio.module.css";
 
 const Bio = () => {
     return (
-        <div className={classes.bio}>
+        <div className={classes.bio} id="bio">
             <p>
                 I recently completed my service in the IDF Medical Corps where I worked as full stack developer / medical procurement engineer. During
                 my service, I developed two web applications for the IDF that are at use, and spent my free time working on my own application for
--- src/components/content/Header.js
+++ src/components/content/Header.js
@@ -3,10 +3,75 @@ import SvgGithubIcon from "../../generatedIcons/GithubIcon";
 import SvgLinkedInIcon from "../../generatedIcons/LinkedInIcon";
 import classes from "./Header.module.css";
 import Pdf from "../../CV-LiorKoren.pdf";
+import { useEffect, useRef, useState } from "react";
+import { Transition } from "react-transition-group";
+
+const miniHeaderOpacities = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+};
+
+const MiniHeader = ({ upon }) => {
+    const nodeRef = useRef(null);
+    return (
+        <Transition nodeRef={nodeRef} in={upon} timeout={0}>
+            {(state) => (
+                <div
+                    ref={nodeRef}
+                    className={classes.topBar}
+                    style={miniHeaderOpacities[state]}
+                    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                >
+                    <img src="/assets/AvatarLiorKoren.jpg" alt="lior koren avatar" />
+                    <h1>Lior Koren</h1>
+                </div>
+            )}
+        </Transition>
+    );
+};
 
 const Header = () => {
+    const [scrolledDown, setScrolledDown] = useState(false);
+    const [reload, setReload] = useState(false);
+    const isMobile = window.innerWidth <= 700;
+
+    console.log("RELOAD APPENED");
+
+    useEffect(() => {
+        const declareScrolledDown = () => {
+            if (window.scrollY > window.innerHeight - 200) {
+                setScrolledDown(true);
+            } else {
+                setScrolledDown(false);
+            }
+        };
+        const forceReload = () => setReload((prev) => !prev);
+        window.addEventListener("resize", forceReload);
+        if (!isMobile) {
+            window.removeEventListener("scroll", declareScrolledDown);
+            setScrolledDown(false);
+        } else {
+            // if in Mobile Version:
+            const headerChildren = [...document.getElementById("header").childNodes];
+            const leftoverHeight = window.innerHeight - headerChildren.slice(1, 4).reduce((prev, curr) => prev + curr.clientHeight, 0);
+            console.log(leftoverHeight);
+            document.getElementById("mobileClicker").setAttribute("style", `height: calc(${leftoverHeight}px - 3rem)`);
+            console.log(`H: ${document.getElementById("mobileClicker").clientHeight}`);
+            window.addEventListener("scroll", declareScrolledDown);
+            return () => {
+                window.removeEventListener("scroll", declareScrolledDown);
+            };
+        }
+        return () => {
+            window.removeEventListener("resize", forceReload);
+        };
+    }, [isMobile]);
+
     return (
-        <header className={classes.header}>
+        <header className={classes.header} id="header">
+            <MiniHeader upon={scrolledDown} />
             <img src="/assets/AvatarLiorKoren.jpg" alt="lior koren avatar" />
             <div className={classes.details}>
                 <h1>Lior Koren</h1>
@@ -24,6 +89,18 @@ const Header = () => {
                     <SvgCvIcon />
                 </a>
             </div>
+            {isMobile && (
+                <div id="mobileClicker" className={classes.mobileClicker}>
+                    <h1 onClick={() => window.scrollTo({ top: window.innerHeight - 130, behavior: "smooth" })}>Bio</h1>
+                    <h1
+                        onClick={() =>
+                            window.scrollTo({ top: window.innerHeight + document.getElementById("bio").clientHeight - 50, behavior: "smooth" })
+                        }
+                    >
+                        Projects
+                    </h1>
+                </div>
+            )}
         </header>
     );
 };
